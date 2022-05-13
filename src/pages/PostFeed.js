189e4cb101054c3906a12b467c3d498ac8db3b97
ngf93 <nabiullinaguzel93@gmail.com>
2022-05-13 12:30:25 +0300

    Feature: PostFeed - post block in process

--- src/pages/PostFeed.js
+++ src/pages/PostFeed.js
@@ -4,6 +4,8 @@ import { Navigation, FreeMode } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/free-mode';
+import TextAbout from '../components/utilities/TextAbout';
+import { IoChevronDown } from "react-icons/io5";
 
 export default function PostFeed() {
     return (
@@ -122,21 +124,75 @@ export default function PostFeed() {
             <section className="row">
                 <div className="col-7">
                     <div className="post-card">
-                        <div className="top">
+                        <div className="top mb-3">
                             <div className="d-flex align-items-center">
                                 <div className="photo empty">
                                     <img src="../img/users/empty-photo.svg" alt="Пользователь"/>
                                 </div>
-                                <div>
+                                <div className="ms-3">
                                     <div>@evgenievaaa</div>
                                     <div>Вероника Евгеньева</div>
                                 </div>
                             </div>
-                            <div className="d-flex align-items-center">
-                                <button type="button">
+                            <ul className="list-unstyled d-flex align-items-center">
+                                <li>
+                                    <button type="button" data-bs-toggle="modal" data-bs-target="#announcement">
+                                        <img src="../img/icons/ribbon.png" alt="Закладки"/>
+                                    </button>
+                                </li>
+                                <li className="ms-3">
+                                    <button type="button" data-bs-toggle="modal" data-bs-target="#announcement">
+                                        <img src="../img/icons/user-add.png" alt="Добавить в подписки"/>
+                                    </button>
+                                </li>
+                                <li className="ms-2">
+                                    <button type="button" data-bs-toggle="modal" data-bs-target="#announcement">
+                                        <img src="../img/icons/drop-menu.png" alt="Меню"/>
+                                    </button>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="main mb-3">
+                            <img src="../img/img1.png" alt="post" className="post-img"/>
+                            <ul className="list-unstyled">
+                                <li>
+                                    <button type="button" data-bs-toggle="modal" data-bs-target="#announcement">
+                                        <img src="../img/icons/likes.png" alt="Нравится"/>
+                                    </button>
+                                    <div className="fs-09">123</div>
+                                </li>
+                                <li>
+                                    <button type="button" data-bs-toggle="modal" data-bs-target="#announcement">
+                                        <img src="../img/icons/messages.png" alt="Комментарии"/>
+                                    </button>
+                                    <div className="fs-09">123</div>
+                                </li>
+                                <li>
+                                    <button type="button" data-bs-toggle="modal" data-bs-target="#announcement">
+                                        <img src="../img/icons/share.png" alt="Поделиться"/>
+                                    </button>
+                                    <div className="fs-09">1.1k</div>
+                                </li>
+                                <li>
+                                    <button type="button">
+                                        <img src="../img/icons/views.png" alt="Просмотры"/>
+                                    </button>
+                                    <div className="fs-09">18.1k</div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="descr">
+                            <TextAbout className="mt-2 mt-sm-4" text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat'} />
+                        </div>
+                        <div className="comments-box">
+                            <button type="button" className="blue">
+                                Сначала интересные
+                                <IoChevronDown />
+                            </button>
+                            <div className="comments">
 
-                                </button>
                             </div>
+                            <button type="button" className="blue">Посмотреть все комментарии</button>
                         </div>
                     </div>
                 </div>
